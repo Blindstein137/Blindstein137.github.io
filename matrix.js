f220d918ba1cacf148a2ce4b49f6beb5d88e6b79
Blindstein137 <Cullyomeara@gmail.com>
2025-06-10 01:30:51 -0400

    Update matrix.js
    Undefined Vec3. Now defined as class

--- matrix.js
+++ matrix.js
@@ -1,79 +1,105 @@
-export class Mat3 {
-  constructor(...args) {
-    for (let i = 0; i < 9; i++) this[i] = args[i] || 0;
+// matrix.js (final validated + Vec3-aware version)
+
+class Vec3 {
+  constructor(x, y, z = 1) {
+    this[0] = x;
+    this[1] = y;
+    this[2] = z;
+  }
+
+  *[Symbol.iterator]() {
+    yield this[0];
+    yield this[1];
+    yield this[2];
+  }
+
+  toString() {
+    return `Vec3(${this[0]}, ${this[1]}, ${this[2]})`;
+  }
+}
+
+class Mat3 {
+  constructor(data) {
+    this.data = data;
   }
 
   static identity() {
-    return new Mat3(
+    return new Mat3([
       1, 0, 0,
       0, 1, 0,
-      0, 0, 1
-    );
+      0, 0, 1,
+    ]);
   }
 
-  static translate(x, y) {
-    return new Mat3(
-      1, 0, 0,
-      0, 1, 0,
-      x, y, 1
-    );
+  static translate([tx, ty]) {
+    return new Mat3([
+      1, 0, tx,
+      0, 1, ty,
+      0, 0, 1,
+    ]);
   }
 
-  static scale2(sx, sy) {
-    return new Mat3(
+  static scale2([sx, sy]) {
+    return new Mat3([
       sx, 0, 0,
       0, sy, 0,
-      0, 0, 1
-    );
+      0, 0, 1,
+    ]);
   }
 
-  static rotate(angle) {
-    const c = Math.cos(angle), s = Math.sin(angle);
-    return new Mat3(
-      c, s, 0,
-     -s, c, 0,
-      0, 0, 1
-    );
+  static rotate(theta) {
+    const c = Math.cos(theta);
+    const s = Math.sin(theta);
+    return new Mat3([
+      c, -s, 0,
+      s,  c, 0,
+      0,  0, 1,
+    ]);
   }
 
-  mul(y) {
-    if (y instanceof Vec3) {
+  mul(other) {
+    if (other instanceof Vec3) {
+      const m = this.data;
+      const [x, y, z] = other;
       return new Vec3(
-        y[0]*this[0] + y[1]*this[3] + y[2]*this[6],
-        y[0]*this[1] + y[1]*this[4] + y[2]*this[7],
-        y[0]*this[2] + y[1]*this[5] + y[2]*this[8]
+        m[0] * x + m[1] * y + m[2] * z,
+        m[3] * x + m[4] * y + m[5] * z,
+        m[6] * x + m[7] * y + m[8] * z
       );
     }
-    const r = new Array(9).fill(0);
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        for (let k = 0; k < 3; k++) {
-          r[i*3 + j] += this[i*3 + k] * y[k*3 + j];
+    const a = this.data;
+    const b = other.data;
+    const result = new Array(9);
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        result[row * 3 + col] = 0;
+        for (let i = 0; i < 3; i++) {
+          result[row * 3 + col] += a[row * 3 + i] * b[i * 3 + col];
         }
       }
     }
-    return new Mat3(...r);
+    return new Mat3(result);
   }
 
-  inverse() {
-    const [a,b,c,d,e,f,g,h,i] = this;
-    const A = e*i - f*h, B = c*h - b*i, C = b*f - c*e;
-    const D = f*g - d*i, E = a*i - c*g, F = c*d - a*f;
-    const G = d*h - e*g, H = b*g - a*h, I = a*e - b*d;
-    const det = a*A + b*D + c*G;
-    return new Mat3(
-      A/det, B/det, C/det,
-      D/det, E/det, F/det,
-      G/det, H/det, I/det
-    );
+  applyToVec3(vec) {
+    const [x, y, z] = vec;
+    const m = this.data;
+    return [
+      m[0] * x + m[1] * y + m[2] * z,
+      m[3] * x + m[4] * y + m[5] * z,
+      m[6] * x + m[7] * y + m[8] * z,
+    ];
   }
 
-  [Symbol.iterator]() {
-    let index = 0;
-    return {
-      next: () => index < 9
-        ? { value: this[index++], done: false }
-        : { value: undefined, done: true }
-    };
+  *[Symbol.iterator]() {
+    yield* this.data;
+  }
+
+  toString() {
+    return `[
+      ${this.data.slice(0, 3).join(', ')},\n      ${this.data.slice(3, 6).join(', ')},\n      ${this.data.slice(6, 9).join(', ')}
+    ]`;
   }
 }
+
+export { Mat3, Vec3 };
