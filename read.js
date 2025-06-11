197c398afb466604701a0afdaf1dce40ac90c7cf
Blindstein137 <Cullyomeara@gmail.com>
2025-06-11 01:38:12 -0400

    Update read.js
    Scaling and axis drawing fixes

--- read.js
+++ read.js
@@ -1,45 +1,41 @@
 // read.js
-// Builds a transformation matrix from a transform stack and applies it to scene objects
+// Converts descriptor data into drawable instance objects with transform matrices
 
 import { Mat3 } from './matrix.js';
 
-function buildMatrix(stack) {
-  let matrix = Mat3.identity();
-  for (let i = 0; i < stack.length; i++) {
-    const cmd = stack[i];
-    const arg = stack[i + 1];
-    switch (cmd) {
-      case 'translate':
-        matrix = matrix.mul(Mat3.translate(arg));
-        i++;
-        break;
-      case 'scale':
-        matrix = matrix.mul(Mat3.scale2(arg));
-        i++;
-        break;
-      case 'rotate':
-        matrix = matrix.mul(Mat3.rotate(arg));
-        i++;
-        break;
-      default:
-        console.warn("Unknown command:", cmd);
+export function read(descriptor) {
+  const primitives = descriptor.primitives;
+
+  return descriptor.objects.map(obj => {
+    const base = primitives[obj.base];
+    let transform = Mat3.identity();
+
+    for (let i = 0; i < obj.transform.length; i++) {
+      const cmd = obj.transform[i];
+      const arg = obj.transform[i + 1];
+      switch (cmd) {
+        case 'translate':
+          transform = transform.mul(Mat3.translate(arg));
+          i++;
+          break;
+        case 'scale':
+          transform = transform.mul(Mat3.scale2(arg));
+          i++;
+          break;
+        case 'rotate':
+          transform = transform.mul(Mat3.rotate(arg));
+          i++;
+          break;
+        case 'identity':
+          // identity: no-op
+          break;
+      }
     }
-  }
-  return matrix;
-}
 
-export function read(scene) {
-  const { primitives, objects } = scene;
-
-  return objects.map(obj => {
-    const shape = primitives[obj.shape];
-    const transform = buildMatrix(obj.transform || []);
     return {
-      type: shape.type,
+      ...obj,
+      base,
       transform,
-      base: shape,
-      style: obj.style || {},
-      textOverride: obj.textOverride || null
     };
   });
 }
